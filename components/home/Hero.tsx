716bfe4ee697cc267813490c2b4a6a70dd228efd
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-06-03 00:15:09 +0100

    done with hero page

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -4,7 +4,7 @@ const Hero:React.FC = () => {
   return (
       <div>
           <div className='max-w-7xl mx-auto flex bg-green-400 py-10 lg:py-16 mt-16 items-center font-serif relative'>
-            <div className="left px-10 space-y-5 w-[50%]">
+            <div className="left px-10 space-y-5 sm:w-[50%] w-full">
                 <h1 className='text-white text-5xl'>Hello Dev</h1>
                 <p className='text-xl text-gray-100 font-extrabold'>
                     This is a fullstack development blog. 
