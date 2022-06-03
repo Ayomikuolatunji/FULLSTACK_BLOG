7583f47656bf7c220932147776d1d6e3db16f10c
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-06-03 11:33:30 +0100

    fetch single post

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,7 +1,14 @@
+import { GetStaticProps, NextPage } from 'next'
 import React from 'react'
 import { sanityClient } from '../../sanity'
+import Post from '../../typings'
 
-const Slug = () => {
+interface props{
+  post:Post
+}
+
+const Slug:NextPage<props> = ({post}) => {
+  console.log(post);
   return (
     <div>
           <main>
@@ -14,26 +21,51 @@ const Slug = () => {
 export default Slug
 
 
-export const getStaticProps=async()=>{
+export const getStaticPaths=async()=>{
     const query= `*[_type=="post"]{
+        _id,
+        slug {
+          current
+        }
+      }  
+      `
+      const posts=await sanityClient.fetch(query)
+       const paths=posts.map((post:Post)=>({
+        params:{
+          slug:post.slug.current
+      }
+       }))
+      return{
+         paths,
+         fallback: 'blocking'
+      }
+}
+
+
+export const getStaticProps:GetStaticProps = async ({params}) => {
+
+      const query = `*[_type == "post" && slug.current == $slug][0]{
         _id,
         title,
         description,
         slug,
         mainImage,
-        slug,
         author ->{
         name,
         image
-       }
-      }  
-    
-      `
-      const res=await sanityClient.fetch(query)
-    
-      return{
-        props:{
-          posts:res
-        }
+      }
+      }`
+      const post = await sanityClient.fetch(query,{
+        slug:params?.slug
+      })
+      if(!post){
+          return {
+            notFound:true
+          }
+      }
+      return {
+        props: {
+          post,
+        },
       }
 }
